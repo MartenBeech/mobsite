2e376ebedbfa7193e584becd7edf104cbbb5fa4c
Morten Bech <mortenbech22@hotmail.com>
2022-02-03 14:29:35 +0100

    Implemented currentTime

---
+++ src/pages/currentTime.tsx
@@ -0,0 +1,27 @@
+import react, {useState, useEffect} from "react";
+
+export const CurrentTime = (): JSX.Element => {
+    const [state, setState] = useState({
+        date: '', 
+        time: '',
+    });
+
+    useEffect(() => {
+        UpdateTime();
+    }, [])
+
+    const UpdateTime = () => {
+        const now = new Date();
+        const date = now.toLocaleDateString();
+        const time = now.toLocaleTimeString();
+
+        setState ({date: date, time: time})
+    }
+    
+    setInterval(UpdateTime, 1000);
+    return (
+        <div>
+            {`${state.date} ${state.time}`}
+        </div>
+    )
+}
